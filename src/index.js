1255640a23c0fa293ac41cbaf3588628c6952cb2
LawrenceSoftwareCompany <158987367+LawrenceSoftwareCompany@users.noreply.github.com>
2024-02-20 20:51:34 -0700

    First Draft
    First draft upload

--- src/index.js
+++ src/index.js
@@ -1,166 +1,151 @@
-const demoImgElement = document.getElementById("ourWorkImg");
-const ourWorkTextContainer = document.getElementById("ourWorkTextContainer");
-const reviewWrapper = document.getElementById("reviewWrapper");
-let currentWorkDemo = 0;
-let currentReview = 0;
-const workDemos = [
-  {
-    src: "demoImgOne",
-    txt: [
-      `        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
-  quidem modi veritatis? Necessitatibus quis libero quas
-  consequuntur voluptatem velit nihil tenetur distinctio qui
-  quidem ut, voluptate at accusamus incidunt earum. Lorem ipsum
-  dolor sit amet consectetur adipisicing elit. Fuga praesentium
-  tenetur laboriosam, velit corporis nam quas vitae natus libero
-  voluptatem totam laborum impedit debitis blanditiis alias
-  eveniet, qui ipsam repellat.`,
+const clearPage = () => {
+  const content = document.getElementById("content");
 
-      `          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-  Adipisci, quod eos? Maiores asperiores saepe ea quae laboriosam!
-  Exercitationem dignissimos tempora nostrum quis ipsa, culpa
-  labore! Unde minima molestiae illo necessitatibus? Lorem ipsum
-  dolor sit amet consectetur adipisicing elit. Esse aliquid unde
-  sit repellendus corrupti, voluptatum velit repellat pariatur
-  voluptate reiciendis nam porro quam laudantium cum expedita
-  consequuntur iure? Impedit, tempore.`,
-
-      `   Lorem ipsum dolor sit amet consectetur adipisicing elit.
-  Inventore officia dolorem pariatur neque id dolore quasi quis,
-  necessitatibus doloremque fuga illum doloribus provident
-  molestiae facilis nobis eum quia eaque consequuntur?`,
-    ],
-  },
-  {
-    src: "demoImgTwo",
-    txt: [
-      `          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-    Adipisci, quod eos? Maiores asperiores saepe ea quae laboriosam!
-    Exercitationem dignissimos tempora nostrum quis ipsa, culpa
-    labore! Unde minima molestiae illo necessitatibus? Lorem ipsum
-    dolor sit amet consectetur adipisicing elit. Esse aliquid unde
-    sit repellendus corrupti, voluptatum velit repellat pariatur
-    voluptate reiciendis nam porro quam laudantium cum expedita
-    consequuntur iure? Impedit, tempore.`,
-
-      `        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
-    quidem modi veritatis? Necessitatibus quis libero quas
-    consequuntur voluptatem velit nihil tenetur distinctio qui
-    quidem ut, voluptate at accusamus incidunt earum. Lorem ipsum
-    dolor sit amet consectetur adipisicing elit. Fuga praesentium
-    tenetur laboriosam, velit corporis nam quas vitae natus libero
-    voluptatem totam laborum impedit debitis blanditiis alias
-    eveniet, qui ipsam repellat.`,
-
-      `   Lorem ipsum dolor sit amet consectetur adipisicing elit.
-    Inventore officia dolorem pariatur neque id dolore quasi quis,
-    necessitatibus doloremque fuga illum doloribus provident
-    molestiae facilis nobis eum quia eaque consequuntur?`,
-    ],
-  },
-];
-
-const reviews = [
-  {
-    name: "John Doe",
-    stars: 5,
-    review: ` Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
-  cum, itaque modi sed voluptatibus amet obcaecati deleniti eum
-  recusandae non veniam pariatur cupiditate id perspiciatis?
-  Dolorum explicabo sint accusantium quasi.`,
-  },
-  {
-    name: "Jane Doe",
-    stars: 5,
-    review: ` Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
-  cum, itaque modi sed voluptatibus amet obcaecati deleniti eum
-  recusandae non veniam pariatur cupiditate id perspiciatis?
-  Dolorum explicabo sint accusantium quasi.`,
-  },
-  {
-    name: "Jack Doe",
-    stars: 5,
-    review: ` Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
-  cum, itaque modi sed voluptatibus amet obcaecati deleniti eum
-  recusandae non veniam pariatur cupiditate id perspiciatis?
-  Dolorum explicabo sint accusantium quasi.`,
-  },
-];
-
-const cycleWorkDemo = (modifer) => {
-  ourWorkTextContainer.childNodes.forEach((child) => {
-    ourWorkTextContainer.removeChild(child);
-  });
-
-  currentWorkDemo += modifer;
-  if (currentWorkDemo == workDemos.length) {
-    currentWorkDemo = 0;
-  } else if (currentWorkDemo < 0) {
-    currentWorkDemo = workDemos.length - 1;
+  while (content.childNodes.length > 0) {
+    content.removeChild(content.childNodes[0]);
   }
-  demoImgElement.classList = "demoWorkImg " + workDemos[currentWorkDemo].src;
-  workDemos[currentWorkDemo].txt.forEach((txt) => {
-    const p = document.createElement("p");
-    p.textContent = txt;
-    p.classList = "ourWorkTxtBlock";
-    ourWorkTextContainer.appendChild(p);
-  });
 };
 
-const createReview = (index) => {
-  // create current Review
-  const div = document.createElement("div");
-  div.classList = "review";
-  // Create the clients name
-  const clientName = document.createElement("h3");
-  clientName.classList = "clientName";
-  clientName.textContent = reviews[index].name;
-  // Create stars
-  const stars = document.createElement("span");
-  stars.classList = "stars";
-  let i = 0;
-  while (i < reviews[index].stars) {
-    const star = document.createElement("ion-icon");
-    star.name = "star";
-    stars.appendChild(star);
-    i++;
-  }
-  // Create the body of the review
-  const reviewBody = document.createElement("p");
-  reviewBody.classList = "reviewTxt";
-  reviewBody.textContent = reviews[index].review;
-
-  div.append(clientName, stars, reviewBody);
-  return div;
+constructHomePage = () => {
+  const content = document.getElementById("content");
+  const openingSprawl = document.createElement("div");
+  openingSprawl.classList = "openingSprawl";
+  const text = document.createElement("h1");
+  text.textContent = "ZackLawrence.net";
+  openingSprawl.appendChild(text);
+  content.appendChild(openingSprawl);
 };
 
-const cycleReview = (update) => {
-  reviewWrapper.innerHTML = "";
+constructAboutMePage = () => {
+  const content = document.getElementById("content");
 
-  currentReview += update;
-  if (0 > currentReview) {
-    currentReview = reviews.length - 1;
-  }
-  if (currentReview + update > 3) {
-    currentReview = 0;
-  }
-  const focusedReview = createReview(currentReview);
-  focusedReview.classList = "review focusedReview";
-  let leftReview = currentReview - 1;
-  let rightReview = currentReview + 1;
-  if (currentReview == 0) {
-    leftReview = reviews.length - 1;
-  }
-  if (currentReview == reviews.length - 1) {
-    rightReview = 0;
-  }
-  const prevReview = createReview(leftReview);
-  const nxtReview = createReview(rightReview);
-  prevReview.classList = "review unfocusedReview";
-  nxtReview.classList = "review unfocusedReview";
+  // Creating the first Text block
+  const introSection = document.createElement("div");
+  introSection.classList = "sectionContainer";
+  const header = document.createElement("h2");
+  header.textContent = "About Me";
+  header.classList = "sectionHeader";
+  const introP = document.createElement("p");
+  introP.classList = "textBlock";
+  introP.textContent =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam eius reprehenderit aliquam iusto et quibusdam facere perferendis! Libero saepe, corporis, esse est tenetur nobis, provident fugit nesciunt laudantium alias nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repellendus eum officiis, similique nulla ducimus ut tempora fuga itaque praesentium perferendis libero ipsum aliquid placeat eligendi eius ad dolor quisquam?";
+  const introP2 = document.createElement("p");
+  introP2.classList = "textBlock";
+  introP2.textContent =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam eius reprehenderit aliquam iusto et quibusdam facere perferendis! Libero saepe, corporis, esse est tenetur nobis, provident fugit nesciunt laudantium alias nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repellendus eum officiis, similique nulla ducimus ut tempora fuga itaque praesentium perferendis libero ipsum aliquid placeat eligendi eius ad dolor quisquam?";
+  introSection.append(header, introP, introP2);
 
-  reviewWrapper.append(prevReview, focusedReview, nxtReview);
+  // Creating first polaroid
+  const frame1 = document.createElement("div");
+  frame1.classList = "photoFrame";
+  const img1 = document.createElement("img");
+  img1.classList = "gridSectionPhoto";
+  img1.src = "../assets/images/bridge.jpg";
+  frame1.appendChild(img1);
+
+  // Second Text block
+  const sectionTwo = document.createElement("div");
+  sectionTwo.classList = "sectionContainer";
+  const sectionTwoHeader = document.createElement("h2");
+  sectionTwoHeader.textContent = "Hobbies";
+  sectionTwoHeader.classList = "sectionHeader";
+  const sectionTwoP = document.createElement("p");
+  sectionTwoP.classList = "textBlock";
+  sectionTwoP.textContent =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam eius reprehenderit aliquam iusto et quibusdam facere perferendis! Libero saepe, corporis, esse est tenetur nobis, provident fugit nesciunt laudantium alias nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repellendus eum officiis, similique nulla ducimus ut tempora fuga itaque praesentium perferendis libero ipsum aliquid placeat eligendi eius ad dolor quisquam?";
+  const sectionTwoP2 = document.createElement("p");
+  sectionTwoP2.classList = "textBlock";
+  sectionTwoP2.textContent =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam eius reprehenderit aliquam iusto et quibusdam facere perferendis! Libero saepe, corporis, esse est tenetur nobis, provident fugit nesciunt laudantium alias nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repellendus eum officiis, similique nulla ducimus ut tempora fuga itaque praesentium perferendis libero ipsum aliquid placeat eligendi eius ad dolor quisquam?";
+
+  sectionTwo.append(sectionTwoHeader, sectionTwoP, sectionTwoP2);
+  // Second polaroid
+  const frame2 = document.createElement("div");
+  frame2.classList = "photoFrame";
+  const img2 = document.createElement("img");
+  img2.classList = "gridSectionPhoto";
+  img2.src = "../assets/images/hobbies.jpg";
+  frame2.appendChild(img2);
+
+  content.append(introSection, frame1, frame2, sectionTwo);
 };
 
-cycleWorkDemo(0);
-cycleReview(0);
+constructAchievementsPage = () => {
+  const content = document.getElementById("content");
+
+  // Creating the first Text block
+  const introSection = document.createElement("div");
+  introSection.classList = "sectionContainer";
+  const header = document.createElement("h2");
+  header.textContent = "Achievements";
+  header.classList = "sectionHeader";
+  const introP = document.createElement("p");
+  introP.classList = "textBlock";
+  introP.textContent =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam eius reprehenderit aliquam iusto et quibusdam facere perferendis! Libero saepe, corporis, esse est tenetur nobis, provident fugit nesciunt laudantium alias nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repellendus eum officiis, similique nulla ducimus ut tempora fuga itaque praesentium perferendis libero ipsum aliquid placeat eligendi eius ad dolor quisquam?";
+  const introP2 = document.createElement("p");
+  introP2.classList = "textBlock";
+  introP2.textContent =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam eius reprehenderit aliquam iusto et quibusdam facere perferendis! Libero saepe, corporis, esse est tenetur nobis, provident fugit nesciunt laudantium alias nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repellendus eum officiis, similique nulla ducimus ut tempora fuga itaque praesentium perferendis libero ipsum aliquid placeat eligendi eius ad dolor quisquam?";
+  introSection.append(header, introP, introP2);
+
+  // Creating first polaroid
+  const frame1 = document.createElement("div");
+  frame1.classList = "photoFrame";
+  const img1 = document.createElement("img");
+  img1.classList = "gridSectionPhoto";
+  img1.src = "../assets/images/Pancho.jpg";
+  frame1.appendChild(img1);
+
+  // Second Text block
+  const sectionTwo = document.createElement("div");
+  sectionTwo.classList = "sectionContainer";
+  const sectionTwoHeader = document.createElement("h2");
+  sectionTwoHeader.textContent = "Awards";
+  sectionTwoHeader.classList = "sectionHeader";
+  const sectionTwoP = document.createElement("p");
+  sectionTwoP.classList = "textBlock";
+  sectionTwoP.textContent =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam eius reprehenderit aliquam iusto et quibusdam facere perferendis! Libero saepe, corporis, esse est tenetur nobis, provident fugit nesciunt laudantium alias nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repellendus eum officiis, similique nulla ducimus ut tempora fuga itaque praesentium perferendis libero ipsum aliquid placeat eligendi eius ad dolor quisquam?";
+  const sectionTwoP2 = document.createElement("p");
+  sectionTwoP2.classList = "textBlock";
+  sectionTwoP2.textContent =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam eius reprehenderit aliquam iusto et quibusdam facere perferendis! Libero saepe, corporis, esse est tenetur nobis, provident fugit nesciunt laudantium alias nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repellendus eum officiis, similique nulla ducimus ut tempora fuga itaque praesentium perferendis libero ipsum aliquid placeat eligendi eius ad dolor quisquam?";
+
+  sectionTwo.append(sectionTwoHeader, sectionTwoP, sectionTwoP2);
+  // Second polaroid
+  const frame2 = document.createElement("div");
+  frame2.classList = "photoFrame";
+  const img2 = document.createElement("img");
+  img2.classList = "gridSectionPhoto";
+  img2.src = "assets/images/podium.jpg";
+  frame2.appendChild(img2);
+
+  content.append(frame1, introSection, sectionTwo, frame2);
+};
+
+const updateBackGround = (page) => {
+  const body = document.getElementById("body");
+  clearPage();
+
+  if (page == "home") {
+    window.location.href = "#top";
+    body.classList = "homePage";
+    content.classList = "wrapper centerContent";
+    constructHomePage();
+  } else if (page == "aboutMe") {
+    window.location.href = "#top";
+
+    body.classList = "aboutMePage";
+    content.classList = "wrapper twoColumnGrid";
+    constructAboutMePage();
+  } else if (page == "achievements") {
+    window.location.href = "#top";
+
+    body.classList = "achievementsPage";
+    content.classList = "wrapper twoColumnGrid";
+    constructAchievementsPage();
+  } else {
+    console.log("404: \n Page Not Found");
+  }
+};
